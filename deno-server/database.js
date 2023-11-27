533f6e5a9307b72ed6a146aa80ca42675095651b
Joel Häkkinen <joel.hakkinen@gmail.com>
2023-11-27 13:54:03 +0200

    fix dockerfiles

--- deno-server/database.js
+++ deno-server/database.js
@@ -1,7 +1,7 @@
 import { postgres } from "./deps.js";
 console.log(Deno.args[0])
 const sql = Deno.args[0] === "production"
-  ? postgres(Deno.env.get("PGURL"), { max: 2 })
+  ? postgres(Deno.env.get("ELEPHANTSQL_URL"), { max: 2 })
   : postgres({ max: 2 });
 
 export { sql };
